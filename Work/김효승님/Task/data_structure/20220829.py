189f6486c77c162234497afcc405925a974f2318
leekyeyoon <sutheacine@naver.com>
2022-09-01 17:13:19 +0900

    20220901

--- Work/김효승님/Task/data_structure/20220829.py
+++ Work/김효승님/Task/data_structure/20220829.py
@@ -1,51 +1,62 @@
 import random
 
-def 판정(너, 상대): /aaaaaa
-    if 너=='묵':
-        if 상대=='묵':
-            승패='비김'
-        elif 상대=='찌':
-            승패='이김'
-        else:
-            승패='짐'
-    elif 너=='찌':
-        if 상대=='묵':
-            승패='짐'
-        elif 상대=='찌':
-            승패='비김'
-        else:
-            승패='이김'
-    elif 너=='빠':
-        if 상대=='묵':
-            승패='이김'
-        elif 상대=='찌':
-            승패='짐'
-        else:
-            승패='비김'
-    return 승패    #승패는 '나'의 기준으로 작성됨
+def 판정(self, enemy):
+    if self=='묵':
+        if enemy=='묵':
+            Result='비김'
 
-승패 = '비김'
+        elif enemy=='찌':
+            Result='이김'
+
+        else:
+            Result='짐'
+
+    elif self=='찌':
+        if enemy=='묵':
+            Result='짐'
+
+        elif enemy=='찌':
+            Result='비김'
+
+        else:
+            Result='이김'
+
+    elif self=='빠':
+        if enemy=='묵':
+            Result='이김'
+
+        elif enemy=='찌':
+            Result='짐'
+            
+        else:
+            Result='비김'
+    return Result    #Result는 '나'의 기준으로 작성됨
+
+
+Result = '비김'
 묵찌빠 = ['묵','찌','빠']
 
-while 승패 == '비김':
-    너 = input('나 : ')
-    상대 = random.choice(묵찌빠)
-    print("상대 :",상대)
-    승패 = 판정(너, 상대)
-    if 승패 == '비김':
+
+
+while Result == '비김':
+    self = input('나 : ')
+    enemy = random.choice(묵찌빠)
+    print("enemy :",enemy)
+    Result = 판정(self, enemy)
+    if Result == '비김':
         print("가위바위보에서 비겼습니다.\n")
 
-while 승패 != '비김':
-    if 승패 == '이김':
+while Result != '비김':
+    if Result == '이김':
         print("당신이 가위바위보에서 이겼습니다.\n")
     else:
-        print("상대가 가위바위보에서 졌습니다.\n")
-    전판 = 승패
+        print("enemy가 가위바위보에서 졌습니다.\n")
+    전판 = Result
         
-    너 = input('나 : ')
-    상대 = random.choice(묵찌빠)
-    print("상대 :",상대)
-    승패 = 판정(너, 상대)
+    self = input('나 : ')
+    enemy = random.choice(묵찌빠)
+    print("enemy :",enemy)
+    Result = 판정(self, enemy)
 
 if 전판 == '이김':
     print("묵찌빠 게임을 이겼습니다.")
