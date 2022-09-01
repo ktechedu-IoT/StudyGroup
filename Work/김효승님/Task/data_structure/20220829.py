995566f0497d9a50cae848d579f58abc9df8b157
khs <reblank132@naver.com>
2022-09-01 13:23:17 +0900

    20220901

--- Work/김효승님/Task/data_structure/20220829.py
+++ Work/김효승님/Task/data_structure/20220829.py
@@ -1,53 +1,39 @@
 import random
 
-def 판정(너, 상대): /aaaaaa
-    if 너=='묵':
-        if 상대=='묵':
-            승패='비김'
-        elif 상대=='찌':
-            승패='이김'
-        else:
-            승패='짐'
-    elif 너=='찌':
-        if 상대=='묵':
-            승패='짐'
-        elif 상대=='찌':
-            승패='비김'
-        else:
-            승패='이김'
-    elif 너=='빠':
-        if 상대=='묵':
-            승패='이김'
-        elif 상대=='찌':
-            승패='짐'
-        else:
-            승패='비김'
+def game(me, you):
+    me1 = ["묵","찌","빠"]
+    you1 = ["빠","묵","찌"]
+    if me== you:
+        승패='비김'
+    elif me not in me1:
+        승패='잘못'
+    elif me1.index(me) == you1.index(you):
+        승패='졌'
+    else:
+        승패='이김'
     return 승패    #승패는 '나'의 기준으로 작성됨
 
 승패 = '비김'
 묵찌빠 = ['묵','찌','빠']
-
-while 승패 == '비김':
-    너 = input('나 : ')
-    상대 = random.choice(묵찌빠)
-    print("상대 :",상대)
-    승패 = 판정(너, 상대)
+승=0
+무=0
+패=0
+while 1 :
+    me = input('나 : ')
+    you = random.choice(묵찌빠)
+    print("상대 :",you)
+    승패 = game(me, you)
     if 승패 == '비김':
         print("가위바위보에서 비겼습니다.\n")
-
-while 승패 != '비김':
-    if 승패 == '이김':
+        무+=1
+    elif 승패 == '이김':
         print("당신이 가위바위보에서 이겼습니다.\n")
+        승+=1
+    elif 승패 == '잘못':
+        print("잘못입력하였습니다. 종료하겠습니다")
+        print("총판수:"+str(승+패+무)+" "+str(승)+ "승:"+ str(무)+"무" +str(패)+"패")
+        break
     else:
         print("상대가 가위바위보에서 졌습니다.\n")
-    전판 = 승패
-        
-    너 = input('나 : ')
-    상대 = random.choice(묵찌빠)
-    print("상대 :",상대)
-    승패 = 판정(너, 상대)
-
-if 전판 == '이김':
-    print("묵찌빠 게임을 이겼습니다.")
-else:
-    print("묵찌빠 게임에서 졌습니다.")
+        패+=1
+   
