7b6a956937375bcc2e6a6cb8e843f1ccf91d78c2
ahabangu <reblank132@naver.com>
2022-09-03 00:45:56 +0900

    수정중

--- Work/김효승님/Task/data_structure/20220902.py
+++ Work/김효승님/Task/data_structure/20220902.py
@@ -20,10 +20,8 @@ calcu(num)
 
 '''
 
+''' 2차 
 # 사칙연산 인덱스 구하기
-from re import A
-
-
 def arithmetic(list,count):
     sign=[]
     test= ['+','-','/','*']
@@ -44,25 +42,34 @@ def convers(num):
             numberring.append(int(conver))
     numberring.append(int(conver))
     return numberring
-'''
+
+#우선순위
+def Priority(nums):
+    sign = arithmetic(nums)
+    resize=[]
+    for i in sign:
+            if nums[i]=='*':
+                resize.append(i)
+            elif nums[i] == '/':
+                resize.append(i)]
+    for i in sign:
+        if resize not in sign
+            resize.append(i)
 #계산하기
 def calcu(nums):
     number=convers(nums)
-    arithm=arithmetic(nums)
+    arithm= Priority
     result = 0
     for i in range(0,len(number),2): 
         for j in arithm:
             if nums[j]=='*' or '%':
                 if nums[j]=='*':
                     result = number[i] * number[i+1]
-                elif nums[j] == '%':
-                    result = number[i] % number[i+1]
+                elif nums[j] == '/':
+                    result = number[i] / number[i+1]
             elif
-'''
 
-nums = list(input())
-a =[]
-a.append(int(5))
-a.append('+')
-a.append(int(6))
-print(a)
+'''
+#수정 필요사항
+#사칙연산 수정시 숫자 순서 맞춰서 변경이 필요 (해결시 패턴 5)
+#패턴 7 8 숫자 나누기에서 처리 하기
