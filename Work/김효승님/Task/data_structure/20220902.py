c2f2f779b851321f30893892a0e4b69d9a558fe4
khs <reblank132@naver.com>
2022-09-05 11:22:22 +0900

    v0.4

--- Work/김효승님/Task/data_structure/20220902.py
+++ Work/김효승님/Task/data_structure/20220902.py
@@ -1,7 +1,16 @@
-#공백 제거 및 오류 코드
+#공백 제거 및 오류 코드 및 2진수 변환
 def Blank(nums):
     remove_set=' '
+    tmp=''
     blank=[i for i in nums if i not in remove_set]
+    if blank[0]=='b':
+        for j in blank[1::]:
+            if j.isalpha():
+                return 'a'
+            else :
+                tmp += j
+            b = format(int(tmp), 'b')
+        return 'c',b
     for j in blank:
         if j.isalpha():
            return 'a'
@@ -11,28 +20,20 @@ def Blank(nums):
     
     return blank
 
-# 사칙연산 구하기
-def arithmetic(list):
-    count = len(list)
-    sign=[]
-    test= ['+','-','/','*']
-    for i in range(0,count,1):
-        if list[i] in test:   
-            sign.append(list[i])
-    return sign
-
-# 숫자 나누기
+# 숫자 분류
 def convers(num):
     numberring = []
+    sign=[]
     conver = ''
     for k in num:
         if k.isdigit():
             conver+=k
         else:
             numberring.append(int(conver))
+            sign.append(k)
             conver=''
     numberring.append(int(conver))
-    return numberring
+    return numberring,sign
 
 #계산하기
 def calcu(nums):
@@ -41,12 +42,15 @@ def calcu(nums):
         return "non-numeric value Error"
     elif blank=='b':
         return "divided by zero Error"
-    number=convers(blank)
-    arithm=arithmetic(blank)
+    elif blank[0]=='c':
+        return blank[1]
+    number=convers(blank)[0]
+    arithm=convers(blank)[1]
     result=0
     j=0
     k=0
     if k ==0 :
+        #곱하기 나누기 먼저계산
         for i in range(0,len(arithm),1): 
             if arithm[i]=='*' or '/' :
                 if arithm[i]=='*':
@@ -64,8 +68,9 @@ def calcu(nums):
                     k+=1
                     break
     if k ==0 :
-        for i in range(0,len(arithm),1): 
-            if k==0 and arithm[i]=='+' or '-' :
+        #더하기 빼기 먼저계산
+        for l in range(0,len(arithm),1): 
+            if k==0 and arithm[l]=='+' or '-' :
                 if arithm[i]=='+':
                     result = number[i] + number[i+1]
                     number.pop(i+1)
@@ -80,24 +85,29 @@ def calcu(nums):
                 arithm.pop(i)
                 k+=1
                 break
-    while not number==[] and k==1:
-        if arithm[j]=='*':
-            result *=number[j]
-            number.pop(j)
-            arithm.pop(j)
-        elif arithm[j] == '/':
-            result /= number[j]
-            number.pop(j)
-            arithm.pop(j)
-        elif arithm[j] == '+':
-            result += number[j]
-            number.pop(j)
-            arithm.pop(j)
-        elif arithm[j] == '-':
-            result -= number[j]
-            number.pop(j)
-            arithm.pop(j)
-        j+=1
+    while not number ==[] and k==1:
+        #곱하기 계산
+        for i in range(0,len(arithm),1):
+            if arithm[i]=='*':
+                result *=number[i]
+                number.pop(i)
+                arithm.pop(i)
+                break
+            elif arithm[i] == '/':
+                result /= number[i]
+                number.pop(i)
+                arithm.pop(i)
+                break
+        if  '*' or '/' not in number :
+            #더하기 빼기 계산
+            if arithm[j] == '+':
+                result += number[j]
+                number.pop(j)
+                arithm.pop(j)
+            elif arithm[j] == '-':
+                result -= number[j]
+                number.pop(j)
+                arithm.pop(j)
     if type(result)==float:
         return int(result * (10**4))/(10**4)
     return result
