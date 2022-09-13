bd31c277299afec79fcdb57d179011a66cb7b27a
kweon-oj <112043167+kweon-oj@users.noreply.github.com>
2022-09-13 16:06:41 +0900

    error

---
+++ Work/권오준님/1.py
@@ -0,0 +1,41 @@
+import random 
+
+students_a = ['강경식', '권명은', '권오준', '공혁', '김효승', '박제영', '이계윤', '조한근', '허서연']
+students_b = []
+answers_c = ['네','안 온 거 같아요~']
+answers_d = []
+answers_e =[]
+
+yesno = random.choice(answers_c)
+
+i=0
+while 1:
+    student = input()
+    if student in students_a:
+        if student not in students_b :
+            print(yesno)
+            students_b.append(student)
+            answers_d.append(yesno)   
+            if student =='허서연': 
+                break
+        elif student in students_b:
+            print('이미 부르셨어요~') 
+    elif student not in students_a:
+            print('?')
+
+print('-------------<출석명단>-------------')            
+for i in range(len(students_b)):
+    if answers_d[i]=='네':
+        answers_e.append('O')
+    elif answers_d[i]=='안 온 거 같아요~':
+        answers_e.append('X')              
+    print(f'{students_b[i]} : {answers_e[i]}')
+print('-----------------------------------')
+
+
+# 대답이 계속 랜덤으로 뜨지 않고 첫번째 대답이 계속 반복.. why?
+        
+   
+
+
+  
