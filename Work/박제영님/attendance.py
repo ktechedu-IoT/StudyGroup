80f563f1bf6020393d80759dbe6640ff18c9ad8c
moon3612 <moon3612@hanmail.net>
2022-09-15 16:33:51 +0900

    20220915

---
+++ Work/박제영님/attendance.py
@@ -0,0 +1,18 @@
+
+import datetime
+
+i=attendance
+j=late attendence
+k=absent
+attendance=[`강경식`,`권명은`,`권오준`,`공혁`,`김효승`,`박제영`,`이계윤`,`조한근`,`허서연`]
+
+if i datetime <= 09:10
+     print(i)
+elif i datetime > 09:10
+     remove i[]
+elif i datetime != 09:10
+     remove j[]
+     
+     print(j)
+     print(k)
+    
