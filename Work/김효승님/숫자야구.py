6cc4ab8697e3e36c0f409d6ebf7f2d5b6450e700
khs <reblank132@naver.com>
2022-09-16 16:32:09 +0900

    숫자야구

---
+++ Work/김효승님/숫자야구.py
@@ -0,0 +1,46 @@
+import random
+
+end=0
+
+def count(num,b):
+    s=0
+    c=0
+    o=0
+    global end
+    for i in range(0,3,1):
+        if num[i] == b[i]:
+            s+=1
+        else:
+            for j in range(0,3,1):
+                if num[i] == b[j]:
+                    c+=1
+                else:
+                    o+=1
+    if o==9:
+        end+=1
+    if s==3:
+        return print("승리")
+    if end==3:
+        return print("3진 아웃",b)
+    return print(s,"s ",c,"b ",end,"o")
+
+
+
+b= random.sample([0,1,2,3,4,5,6,7,8,9],3)
+while True:
+    num = list(map(int,input("숫자 3개 입력해주세요: ")))
+    c=count(num,b)
+    if c == "승리":
+        break
+    
+
+
+
+
+
+
+
+
+
+
+
