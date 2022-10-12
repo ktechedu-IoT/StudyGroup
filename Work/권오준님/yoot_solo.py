b7a473f799b20b27b6167e2a9bb094f7b5ac6fb3
kweon-oj <poto07@naver.com>
2022-10-12 16:07:23 +0900

    yoot_solo

--- Work/권오준님/yoot_solo.py
+++ Work/권오준님/yoot_solo.py
@@ -3,10 +3,10 @@ import random as rd
 users = int(input("몇명이 참가할지 정하시오 : "))
 times = int(input("몇번 던질지 정하시오 : "))
 
-maps = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
-p_1 = [10, 13, 16, 19, 25, 30, 35, 40, 0]
-p_2 = [20, 22, 24, 25, 30, 35, 40, 0]
-p_3 = [30, 28, 27, 26, 25, 30, 35, 40, 0]
+map_a = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+map_b = [10, 13, 16, 19, 25, 30, 35, 40, 0]
+map_c = [20, 22, 24, 25, 30, 35, 40, 0]
+map_d = [30, 28, 27, 26, 25, 30, 35, 40, 0]
 
 a=0
 b=0
@@ -26,7 +26,7 @@ while True:
                 a=0 
                 total.append(0)
             else:
-                total.append(maps[a])
+                total.append(map_a[a])
            
             print('yoot:', yoot_1)
             print('a:',a)
@@ -45,7 +45,7 @@ while True:
                 a=0
                 total.append(0)
             else:
-                total.append(p_1[b])
+                total.append(map_b[b])
             
             print('yoot:', yoot_2)
             print('a:',a)
@@ -64,7 +64,7 @@ while True:
                 a=0
                 total.append(0)
             else:
-                total.append(p_2[c])
+                total.append(map_c[c])
             
             print('yoot:', yoot_3)
             print('a:',a)
@@ -83,7 +83,7 @@ while True:
                 a=0
                 total.append(0)
             else:
-                total.append(p_3[d])
+                total.append(map_d[d])
          
             print('yoot:', yoot_4)
             print('a:',a)
